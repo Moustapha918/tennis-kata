66057d300922ee8a8f6054afc5e1f4023d49c990
moustapha SIDI <moustapha.sidi-el-messioub.ext@veolia.com>
2024-05-13 00:36:56 +0200

    add some tests

--- src/test/java/fr/kata/tennis/usecases/TennisGameUTest.java
+++ src/test/java/fr/kata/tennis/usecases/TennisGameUTest.java
@@ -40,4 +40,45 @@ class TennisGameUTest {
         assertThrows(UnknownPlayerException.class , () -> tennisGame.printScenario(scorerList));
     }
 
+    @Test
+    void should_print_scenario_when_player_B_win_after_remontada_before_deuce() throws UnknownPlayerException {
+        // GIVEN
+        var expectedScenario = List.of(
+                "Player A : 15 / Player B : 0",
+                "Player A : 30 / Player B : 0",
+                "Player A : 30 / Player B : 15",
+                "Player A : 30 / Player B : 30",
+                "Player A : 30 / Player B : 40",
+                "Player B wins the game");
+
+        String remontadaInput = "AABBBB";
+        // WHEN
+        TennisGame sut = new TennisGame();
+        var actualScenario = sut.printScenario(remontadaInput);
+
+        // THEN
+        assertEquals(expectedScenario, actualScenario);
+    }
+
+    @Test
+    void should_print_scenario_when_player_B_win_in_random_scenario() throws UnknownPlayerException {
+
+        // GIVEN
+        var expectedScenario = List.of(
+                "Player A : 15 / Player B : 0", // A score
+                "Player A : 15 / Player B : 15", // B score
+                "Player A : 30 / Player B : 15", // A score
+                "Player A : 30 / Player B : 30", // B score
+                "Player A : 40 / Player B : 30", // A score
+                "Player A wins the game");       // A score
+
+        // WHEN
+        TennisGame sut = new TennisGame();
+        var actualScenario = sut.printScenario("ABABAA");
+
+        // THEN
+        assertEquals(expectedScenario, actualScenario);
+    }
+
+
 }
