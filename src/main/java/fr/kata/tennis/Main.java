394afcf57c80f82d8a2c0ef8c50ff93575a96ec0
moustapha SIDI <moustapha.sidi-el-messioub.ext@veolia.com>
2024-05-13 10:25:03 +0200

    implement main method

--- src/main/java/fr/kata/tennis/Main.java
+++ src/main/java/fr/kata/tennis/Main.java
@@ -1,7 +1,19 @@
 package fr.kata.tennis;
 
+import fr.kata.tennis.usecases.TennisGame;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TennisGame tennisGame = new TennisGame();
+        Scanner scanner = new Scanner(System.in);
+        String gameInput = scanner.nextLine();
+
+        var scenario = tennisGame.printScenario(gameInput);
+
+        for(var roundResult: scenario){
+            System.out.println(roundResult);
+        }
     }
 }
